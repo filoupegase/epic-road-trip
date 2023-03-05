f9e424ef814d519f17201cf62c5e2386d5e2513d
filoupegase <loisoncorentin@yahoo.fr>
2023-03-05 16:17:26 +0100

    push next config

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "627709cc038caab7"; }
+/******/ 		__webpack_require__.h = function() { return "86122cfa1017ea66"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
