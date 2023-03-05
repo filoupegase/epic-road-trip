53e05d015eabb0072d52df3313788cbb5db8ef69
filoupegase <loisoncorentin@yahoo.fr>
2023-03-05 16:47:57 +0100

    mui create theme init

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "86122cfa1017ea66"; }
+/******/ 		__webpack_require__.h = function() { return "23bbfebdc5c921d0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#3861fb',
+        },
+        secondary: {
+            main: '#E95D5D',
+        },
+    },
+});
