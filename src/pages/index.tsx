cb23d7f799fe7b032eb1ea5524729e8088c3f765
filoupegase <loisoncorentin@yahoo.fr>
2023-03-13 12:45:40 +0100

    save

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,16 +7,15 @@ export default function Home() {
         <Paper
             component="form"
             elevation={ 3 }
-            sx={ { p: '5px 4px', display: 'flex', alignItems: 'center', width: 400 } }
-        >
-            <IconButton sx={ { p: '10px' } } aria-label="menu">
-                <SearchIcon color='primary' />
-            </IconButton>
+            sx={ { p: '5px 4px', display: 'flex', alignItems: 'center', width: 400 } }>
             <InputBase
                 sx={ { ml: 1, flex: 1 } }
                 placeholder="Where to?"
                 inputProps={ { 'aria-label': 'search google maps' } }
             />
+            <IconButton sx={ { p: '10px' } } aria-label="menu">
+                <SearchIcon color='primary' />
+            </IconButton>
         </Paper>
     )
 }
