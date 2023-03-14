1fdfc14dcc37b9e7e3bf2f6cabb3e28f201014d5
filoupegase <loisoncorentin@yahoo.fr>
2023-03-14 14:44:37 +0100

    push

--- script/script.sh
+++ script/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script to setup the second git repository for the project
+# Script to setup and deploy the second git repository for the project
 # The second git repository is used for the github action
 
 git remote remove origin
