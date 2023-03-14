14247bf4248978907e4c6d188d2cc6a6be0de089
filoupegase <loisoncorentin@yahoo.fr>
2023-03-14 11:46:50 +0100

    remote

--- script/script.sh
+++ script/script.sh
@@ -3,5 +3,7 @@
 # Script to setup the second git repository for the project
 # The second git repository is used for the github action
 
-#git remote remove origin
+git remote remove origin
+git remote add origin git@github.com:EpitechMscProPromo2024/T-WEB-800-PAR_9.git
+git remote add upstream git@github.com:filoupegase/epic-road-trip.git
 git remote -v
