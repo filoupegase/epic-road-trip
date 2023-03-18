3d01b683f0a3b1f94c7644b5151f0130b5383dea
filoupegase <loisoncorentin@yahoo.fr>
2023-03-18 12:35:39 +0100

    fix hot reload babel config

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,60 +1,55 @@
 import * as React from 'react';
-import { Html, Head, Main, NextScript, DocumentProps } from 'next/document';
-import theme from '../_core/styles/theme';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { DocumentContext, DocumentInitialProps } from 'next/document';
+import { ServerStyleSheet } from 'styled-components'
 import * as config from '../_core/config';
-
-
-interface MyDocumentProps extends DocumentProps {
-    styledComponentsTags: JSX.Element[];
-}
+import theme from '../_core/styles/theme';
 
 //https://nextjs.org/docs/advanced-features/custom-document
-export default function MyDocument({ styledComponentsTags }: MyDocumentProps) {
-    return (
-        <Html lang={ config.siteLocale }>
-            <Head>
-                {/* PWA primary color */ }
-                <meta name="theme-color" content={ theme.palette.primary.main } />
-                <link rel="shortcut icon" href="/favicon.ico" />
-                <meta name="emotion-insertion-point" content="" />
+export default class MyDocument extends Document {
+    static async getInitialProps(
+        ctx: DocumentContext
+    ): Promise<DocumentInitialProps> {
 
-            </Head>
-            <body>
-            <Main />
-            <NextScript />
-            </body>
-        </Html>
-    )
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage;
+
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App) => (props) =>
+                        sheet.collectStyles(<App { ...props } />),
+                })
+
+            const initialProps = await Document.getInitialProps(ctx)
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        { initialProps.styles }
+                        { sheet.getStyleElement() }
+                    </>
+                )
+            }
+        } finally {
+            sheet.seal()
+        }
+    }
+
+    render() {
+        return (
+            <Html lang={ config.siteLocale }>
+                <Head>
+                    {/* PWA primary color */ }
+                    <meta name="theme-color" content={ theme.palette.primary.main } />
+                    <link rel="shortcut icon" href="/favicon.ico" />
+                    <meta name="emotion-insertion-point" content="" />
+                </Head>
+                <body>
+                <Main />
+                <NextScript />
+                </body>
+            </Html>
+        )
+    }
 }
-
-
-// import Document, { DocumentContext, DocumentInitialProps } from 'next/document'
-// import { ServerStyleSheet } from 'styled-components'
-//
-//
-// export default class MyDocument extends Document { static async getInitialProps( ctx: DocumentContext ): Promise<DocumentInitialProps> {
-//         const sheet = new ServerStyleSheet()
-//         const originalRenderPage = ctx.renderPage
-//
-//         try {
-//             ctx.renderPage = () =>
-//                 originalRenderPage({
-//                     enhanceApp: (App) => (props) =>
-//                         sheet.collectStyles(<App { ...props } />),
-//                 })
-//
-//             const initialProps = await Document.getInitialProps(ctx)
-//             return {
-//                 ...initialProps,
-//                 styles: (
-//                     <>
-//                         { sheet.getStyleElement() }
-//                         { initialProps.styles }
-//                     </>
-//                 ),
-//             }
-//         } finally {
-//             sheet.seal()
-//         }
-//     }
-// }
