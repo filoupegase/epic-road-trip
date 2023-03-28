9b79a59177926f16da7e4cfa0231183ac6e03515
icarofr <icaro@gmx.fr>
2023-03-28 12:00:07 +0200

    commented lines that were in the way of compilation

--- app/src/pages/api/amadeus/index.js
+++ app/src/pages/api/amadeus/index.js
@@ -8,11 +8,16 @@ const amadeus = new Amadeus({
 const url =
   "https://api.amadeus.com/v2/shopping/hotel-offers?cityCode=PAR&adults=2&radius=5&currency=USD&priceRange=100-300";
 
-amadeus
-  .request(url)
-  .then((response) => {
-    // console.log(response.data);
-  })
-  .catch((error) => {
-    // console.log(error);
-  });
+// désolé Meryem, cette ligne pose des problèmes donc je l'ai commentée
+// amadeus
+//   .request(url)
+//   .then((response) => {
+//     // console.log(response.data);
+//   })
+//   .catch((error) => {
+//     // console.log(error);
+//   });
+
+export default async (req, res) => {
+  return res.status(200).json({ message: "Hello" });
+};
