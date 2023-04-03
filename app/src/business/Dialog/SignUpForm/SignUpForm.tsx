f032fec527402c96893e2aadf39084a21f9882ed
filoupegase <loisoncorentin@yahoo.fr>
2023-04-03 17:00:34 +0200

    save dev

--- app/src/business/Dialog/SignUpForm/SignUpForm.tsx
+++ app/src/business/Dialog/SignUpForm/SignUpForm.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { SignupForm } from "@/interface";
+import { Button, Box, TextField } from "@mui/material";
 
 
 type SignUpFormProps = {}
@@ -16,13 +17,41 @@ const SignUpForm = ({}: SignUpFormProps) => {
     };
 
     const handleChangePassword = (e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-        e.target.id === initialForm.password
+        e.target.id === 'password'
             ? setInitialForm({ ...initialForm, password: e.target.value })
             : setInitialForm({ ...initialForm, passwordCheck: e.target.value })
     };
 
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+    };
+
     return (
-        <>  </>
+        <form onSubmit={ handleSubmit }>
+            <Box sx={ {
+                display: 'flex',
+                flexDirection: 'column',
+                '& > :not(style)': {
+                    mt: 2,
+                    mb: 2,
+                },
+            } }>
+                <TextField id="email" label="Email" variant="outlined"
+                           type='email' onChange={ handleChangeEmail } value={ email } />
+
+                <TextField id="password" label="Password" variant="outlined" type='password'
+                           onChange={ handleChangePassword } />
+
+                <TextField id="passwordCheck" label="Password Check" variant="outlined" type='password'
+                           onChange={ handleChangePassword } />
+                <Box
+                    display='flex'
+                    justifyContent='flex-end'
+                >
+                    <Button type='submit' variant="contained" disableElevation>Sign Up</Button>
+                </Box>
+            </Box>
+        </form>
     );
 }
 
