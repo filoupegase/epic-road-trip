f7a8c74d2a942bc3f19ce256be3d5233c63c63ac
filoupegase <loisoncorentin@yahoo.fr>
2023-04-03 16:17:12 +0200

    save

--- app/src/_core/services/axios/index.ts
+++ app/src/_core/services/axios/index.ts
@@ -2,5 +2,6 @@ import axios, { AxiosInstance } from 'axios';
 
 
 export const axiosClient: AxiosInstance = axios.create({
-    baseURL: process.env.ORIGIN_DEV
+    baseURL: process.env.ORIGIN_DEV,
+    headers: { "Content-Type": "application/json" },
 })
--- app/src/_core/store/services/auth/index.ts
+++ app/src/_core/store/services/auth/index.ts
@@ -1,14 +1,15 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { LoginForm } from "@/interface";
+import { axiosClient } from "@/_core/services/axios";
+import qs from 'qs';
 
 
-// const userToken = localStorage.getItem('userToken')
-//     ? localStorage.getItem('userToken')
-//     : null
+const userToken = typeof window !== 'undefined' ? localStorage.getItem('userToken')
+    : null
 
 export const initialState = {
     loading: false,
-    // userToken,
+    userToken,
     error: null,
     success: false,
 }
@@ -17,7 +18,12 @@ export const login = createAsyncThunk('auth/login',
     async ({ email, password }: LoginForm,
            { rejectWithValue }): Promise<any> => {
         try {
-
+            const { data } = await axiosClient.post(
+                '/api/user/login/',
+                { email, password }
+            )
+            localStorage.setItem('userToken', data.token);
+            return data;
         } catch (error) {
             console.error(error);
         }
@@ -28,5 +34,21 @@ export const authSlice = createSlice({
     name: 'auth',
     initialState,
     reducers: {},
-    extraReducers: {}
+    extraReducers: (builder) => {
+        builder.addCase(login.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+        });
+        builder.addCase(login.fulfilled, (state, { payload }) => {
+            state.loading = false;
+            state.success = true;
+            console.log('>>>>++', payload);
+            //state.userToken = payload.token;
+        });
+        builder.addCase(login.rejected, (state, { payload }) => {
+            state.loading = false
+            // @ts-ignore
+            state.error = payload
+        });
+    }
 });
